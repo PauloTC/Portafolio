c27f89575b4deb11c11d7d76ea8f74f1b3739df6
Paulo Tejada <paulotejada.123a@gmail.com>
2020-02-25 21:05:44 -0500

    published

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
     manifestThemeColor: '#663399',
     manifestDisplay: 'standalone',
     manifestIcon: 'src/assets/img/website-icon.png',
+    pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
     authorName: 'Paulo Tejada',
     heading: 'Frontend Developer',
     // social
