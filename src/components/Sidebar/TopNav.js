d9ec270ac497b79aafd2c9358c9e20d5f3000d89
Paulo Tejada <paulotejada.123a@gmail.com>
2020-02-26 10:21:21 -0500

    working on slide

--- src/components/Sidebar/TopNav.js
+++ src/components/Sidebar/TopNav.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 export default function TopNav({ title, onMenuClick = () => {} }) {
-  return (
-    <div id="headerToggle">
-      <a
-        href="/#"
-        className="toggle"
-        onClick={e => {
-          e.preventDefault();
-          onMenuClick();
-        }}
-      >
-        {' '}
-      </a>
-    </div>
-  );
+	return (
+		<div id="headerToggle">
+			<div
+				className="toggle"
+				onClick={(e) => {
+					e.preventDefault();
+					onMenuClick();
+				}}
+			>
+				{' '}
+			</div>
+		</div>
+	);
 }
