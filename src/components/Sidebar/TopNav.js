b9f43789a7d0f10ace95bd7e453e2bbad9f8f4c0
Paulo Tejada <paulotejada.123a@gmail.com>
2021-06-29 13:36:15 -0500

    working on images

--- src/components/Sidebar/TopNav.js
+++ src/components/Sidebar/TopNav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export default function TopNav({ title, onMenuClick = () => {} }) {
-	return (
-		<div id="headerToggle">
-			<a
-                className="toggle"
-                href="#"
-				onClick={(e) => {
-					e.preventDefault();
-					onMenuClick();
-				}}
-			>
-				{' '}
-			</a>
-		</div>
-	);
+  return (
+    <div id="headerToggle">
+      <a
+        href="/#"
+        className="toggle"
+        onClick={e => {
+          e.preventDefault();
+          onMenuClick();
+        }}
+      >
+        {' '}
+      </a>
+    </div>
+  );
 }
